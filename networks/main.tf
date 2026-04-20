8289790b56876ab574eeead91fdc1f379d9781ad
agutirre_mcafee <alfonso@alfonso.local>
2026-04-20 12:06:28 -0600

    test

--- networks/main.tf
+++ networks/main.tf
@@ -57,7 +57,7 @@ resource "google_compute_router_nat" "nat_gateway" {
   name                               = "webapp-nat-gateway"
   router                             = google_compute_router.router.name
   region                             = var.region
-  nat_ip_allocate_option             = "MANUAL_ONLY"
+  nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
   subnetwork {
     name = google_compute_subnetwork.subnetwork["webapp-subnetwork"].name
