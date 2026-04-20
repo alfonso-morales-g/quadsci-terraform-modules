a8a890df74b461ea7ca325ea8471a9a9663ecdb7
agutirre_mcafee <alfonso@alfonso.local>
2026-04-20 15:03:32 -0600

    test

--- networks/variables.tf
+++ networks/variables.tf
@@ -13,11 +13,6 @@ variable "vpc_name" {
   type    = string
 }
 
-variable "tags" {
-  default = null
-  type    = list(string)
-}
-
 variable "subnetworks" {
   default = null
   type = map(object({
