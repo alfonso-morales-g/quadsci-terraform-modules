eb6345351b1916810fe6d13454cc349f3010f79f
agutirre_mcafee <alfonso@alfonso.local>
2026-04-20 16:20:56 -0600

    instancemodule

--- networks/variables.tf
+++ networks/variables.tf
@@ -35,7 +35,7 @@ variable "firewall_rules" {
       ports    = optional(list(number))
     })),
     source_ranges      = list(string)
-    source_tags        = list(string)
+    source_tags        = optional(list(string))
     target_tags        = list(string)
     destination_ranges = list(string)
     priority           = number
