9f00587fb3c4c375f65e23e734cf5f5f8e7e2581
agutirre_mcafee <alfonso@alfonso.local>
2026-04-20 11:04:22 -0600

    first commit

---
+++ networks/main.tf
@@ -0,0 +1,44 @@
+resource "google_compute_network" "vpc_network" {
+  project                 = var.project_id
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
+}
+
+resource "google_compute_subnetwork" "subnetwork" {
+  for_each = var.subnetworks
+
+  name          = each.value.name
+  ip_cidr_range = each.value.ip_cidr_range
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_firewall" "vpc_firewall" {
+  for_each = var.firewall_rules
+
+  name    = each.value.name
+  network = google_compute_network.vpc_network.name
+  direction = each.value.direction
+
+  dynamic "allow" {
+    for_each = length(each.value.allow_rules) > 0 ? each.value.allow_rules : []
+    content {
+      protocol = allow.value.protocol
+      ports    = allow.value.ports
+    }
+  }
+
+  dynamic "deny" {
+    for_each = length(each.value.deny_rules) > 0 ? each.value.deny_rules : []
+    content {
+      protocol = deny.value.protocol
+      ports    = deny.value.ports
+    }
+  }
+
+  source_ranges      = each.value.source_ranges
+  destination_ranges = each.value.destination_ranges
+  target_tags        = var.tags
+  priority           = each.value.priority
+}
---
+++ networks/variables.tf
@@ -0,0 +1,46 @@
+variable "project_id" {
+  default = null
+  type    = string
+}
+
+variable "region" {
+  default = null
+  type    = string
+}
+
+variable "vpc_name" {
+  default = null
+  type    = string
+}
+
+variable "tags" {
+  default = null
+  type    = list(string)
+}
+
+variable "subnetworks" {
+  default = null
+  type = map(object({
+    name          = string,
+    ip_cidr_range = string
+  }))
+}
+
+variable "firewall_rules" {
+  default = null
+  type = map(object({
+    name = string,
+    direction = string,
+    allow_rules = list(object({
+      protocol = optional(string),
+      ports    = optional(list(number))
+    })),
+    deny_rules = list(object({
+      protocol = optional(string),
+      ports    = optional(list(number))
+    })),
+    source_ranges      = list(string)
+    destination_ranges = list(string)
+    priority           = number
+  }))
+}
