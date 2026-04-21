ec81fa0efff3a3a0446cec7a5d150a664bd2596b
agutirre_mcafee <alfonso@alfonso.local>
2026-04-21 09:16:05 -0600

    instancemodule

--- networks/main.tf
+++ networks/main.tf
@@ -56,7 +56,7 @@ resource "google_compute_router" "router" {
 
 resource "google_compute_router_nat" "nat_gateway" {
   name                               = "webapp-nat-gateway"
-  router                             = google_compute_router.router.name
+  router                             = "nat-router"
   region                             = var.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
